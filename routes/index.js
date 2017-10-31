46e25e9a36eaae45eb36a68455af2ab5454dfdf8
윤진웅/플랫폼개발팀 <jwloen@iloen.com>
2017-10-31 12:08:24 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -14,19 +14,19 @@ const tracks = [
   {
     artist: '아이유',
     title: '가을 아침',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/96/855/10096855_500.jpg',
+    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/96/855/10096855.jpg',
     link: 'https://melon.do/EOsvusIaG'
   },
   {
     artist: '선미',
     title: '가시나',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/89/089/10089089_500.jpg',
+    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/89/089/10089089.jpg',
     link: 'https://melon.do/gi7hWJEtF'
   },
   {
     artist: '에픽하이',
     title: '연애소설 (Feat. 아이유)',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/101/05/030/10105030_500.jpg',
+    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/101/05/030/10105030.jpg',
     link: 'https://melon.do/w2nksaJVH'
   },
 ]
