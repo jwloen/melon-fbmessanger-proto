a8d8cde9c0c864afc15437c5fc40bd775b1e93fd
윤진웅/플랫폼개발팀 <jwloen@iloen.com>
2017-10-31 11:42:38 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -9,26 +9,28 @@
 import express from 'express';
 
 const router = express.Router();
-
 // fake posts to simulate a database
 const tracks = [
   {
     artist: '아이유',
     title: '가을 아침',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/96/855/10096855_500.jpg/melon/resize/120/quality/80/optimize'
+    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/96/855/10096855_500.jpg/melon/resize/120/quality/80/optimize',
+    link: 'https://melon.do/EOsvusIaG'
   },
   {
     artist: '선미',
     title: '가시나',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/89/089/10089089_500.jpg/melon/resize/120/quality/80/optimize'
+    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/89/089/10089089_500.jpg/melon/resize/120/quality/80/optimize',
+    link: 'https://melon.do/gi7hWJEtF'
   },
   {
     artist: '에픽하이',
     title: '연애소설 (Feat. 아이유)',
-    imgurl
-    : 'http://cdnimg.melon.co.kr/cm/album/images/101/05/030/10105030_500.jpg/melon/resize/120/quality/80/optimize'
+    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/101/05/030/10105030_500.jpg/melon/resize/120/quality/80/optimize',
+    link: 'https://melon.do/w2nksaJVH'
   },
 ]
+
 
 // GET home page
 router.get('/', (_, res) => {
--- views/partials/body.ejs
+++
@@ -1,7 +0,0 @@
-var React = require("react")
-var HelloComponent = React.createClass({
-    render:function(){
-        return React.createElement('h1', null, 'Hello Sandeep');
-    }
-});
-module.exports = HelloComponent;
