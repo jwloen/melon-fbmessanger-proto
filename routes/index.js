da7e0acba48ff8f193222998c6ba30814c87feaa
윤진웅/플랫폼개발팀 <jwloen@iloen.com>
2017-10-31 10:38:39 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,29 @@ import express from 'express';
 
 const router = express.Router();
 
+// fake posts to simulate a database
+const tracks = [
+  {
+    artist: '아이유',
+    title: '가을 아침',
+    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/96/855/10096855_500.jpg/melon/resize/120/quality/80/optimize'
+  },
+  {
+    artist: '선미',
+    title: '가시나',
+    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/89/089/10089089_500.jpg/melon/resize/120/quality/80/optimize'
+  },
+  {
+    artist: '에픽하이',
+    title: '연애소설 (Feat. 아이유)',
+    imgurl
+    : 'http://cdnimg.melon.co.kr/cm/album/images/101/05/030/10105030_500.jpg/melon/resize/120/quality/80/optimize'
+  },
+]
+
 // GET home page
 router.get('/', (_, res) => {
-  res.render('./index', {demo: process.env.DEMO});
+  res.render('./index', {demo: process.env.DEMO, tracks : tracks} );
 });
 
 export default router;
