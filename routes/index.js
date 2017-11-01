63fab3fc851c79acf7fa0186b4880823dbf3db61
윤진웅/플랫폼개발팀 <jwloen@iloen.com>
2017-11-01 15:57:55 +0900

    weview design

--- routes/index.js
+++ routes/index.js
@@ -14,31 +14,31 @@ const tracks = [
   {
     artist: '아이유',
     title: '가을 아침',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/96/855/10096855.jpg',
+    imgurl: 'https://cdnimg.melon.co.kr/cm/album/images/100/96/855/10096855.jpg',
     link: 'https://melon.do/EOsvusIaG'
   },
   {
     artist: '선미',
     title: '가시나',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/89/089/10089089.jpg',
+    imgurl: 'https://cdnimg.melon.co.kr/cm/album/images/100/89/089/10089089.jpg',
     link: 'https://melon.do/gi7hWJEtF'
   },
   {
     artist: '에픽하이',
     title: '연애소설 (Feat. 아이유)',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/101/05/030/10105030.jpg',
+    imgurl: 'https://cdnimg.melon.co.kr/cm/album/images/101/05/030/10105030.jpg',
     link: 'https://melon.do/w2nksaJVH'
   },
   {
     artist: '멜로망스',
     title: '선물',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/100/78/176/10078176.jpg',
+    imgurl: 'https://cdnimg.melon.co.kr/cm/album/images/100/78/176/10078176.jpg',
     link: 'https://melon.do/AgaPhIdmE'
   },
   {
     artist: '트와이스',
     title: 'LIKEY',
-    imgurl: 'http://cdnimg.melon.co.kr/cm/album/images/101/07/114/10107114.jpg',
+    imgurl: 'https://cdnimg.melon.co.kr/cm/album/images/101/07/114/10107114.jpg',
     link: 'https://melon.do/K7h5ZPZgH'
   },
 ]
